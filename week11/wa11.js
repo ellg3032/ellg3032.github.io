60c4c6d91cbae2433a3b27aaa00517ba4bb20108
Elliot Goe <egoe3032@gmail.com>
2025-10-30 16:30:02 -0600

    finally got vscode to work

--- week11/wa11.js
+++ week11/wa11.js
@@ -1,3 +1,20 @@
-const endpoint = "http://www.omdbapi.com/?apikey=[21b1bf27]&"
-const button = document.querySelector(button);
+const endpoint = "http://www.omdbapi.com/?apikey=21b1bf27&"
+const button = document.querySelector("#please");
+
+button.addEventListener("click", func);
+
+async function func() {
+    try {
+        const response = await fetch(endpoint + "t=avengers");
+        if(!response.ok) {
+            throw Error(response.statusText);
+        }
+        const json = await response.json();
+        console.log(json);
+    } catch(err) {
+        console.log(err);
+        alert('Failed to get new trivia');
+    }
+}
+
 
