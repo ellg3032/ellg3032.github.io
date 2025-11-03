bfa198174ac2ba2e543d76b12c0653234e7e2a14
Elliot Goe <egoe3032@gmail.com>
2025-11-02 18:33:50 -0700

    wa11 done

--- week11/wa11.js
+++ week11/wa11.js
@@ -1,20 +1,45 @@
-const endpoint = "http://www.omdbapi.com/?apikey=21b1bf27&"
+const endpoint = "http://www.omdbapi.com/?apikey=21b1bf27&t=";
 const button = document.querySelector("#please");
+const clear_storage = document.getElementById("clear_data");
+const img = document.getElementById("PosterImg");
 
 button.addEventListener("click", func);
+clear_storage.addEventListener("click", clear_data);
+
+window.addEventListener("load", function () {
+  const currentposter = localStorage.getItem("current_poster");
+  img.src = currentposter;
+});
 
 async function func() {
-    try {
-        const response = await fetch(endpoint + "t=avengers");
-        if(!response.ok) {
-            throw Error(response.statusText);
-        }
-        const json = await response.json();
-        console.log(json);
-    } catch(err) {
-        console.log(err);
-        alert('Failed to get new trivia');
+  let title = document.getElementById("search").value;
+  let search_title = "";
+  for (let i = 0; i < title.length; i++) {
+    if (title[i] == " ") {
+      search_title += "+";
+    } else {
+      search_title += title[i];
     }
+  }
+  console.log(search_title);
+  try {
+    const response = await fetch(endpoint + search_title);
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    const json = await response.json();
+    if (json["Error"]) {
+      alert(json["Error"]);
+    } else {
+      img.src = json["Poster"];
+      localStorage.setItem("current_poster", json["Poster"]);
+    }
+  } catch (err) {
+    console.log(err);
+    alert("Failed to get movie");
+  }
 }
 
-
+function clear_data() {
+  localStorage.clear;
+}
