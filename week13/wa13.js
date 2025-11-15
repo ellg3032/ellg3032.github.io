51a3324c8d8f1790c6c3bcae814906ac3a2e30b4
Elliot Goe <egoe3032@gmail.com>
2025-11-15 12:11:31 -0700

    wa13 partway done

---
+++ week13/wa13.js
@@ -0,0 +1,120 @@
+const endpoint = "http://www.omdbapi.com/?apikey=21b1bf27&s=";
+const endpoint2 = "http://www.omdbapi.com/?apikey=21b1bf27&t=";
+const button = document.querySelector("#searchbtn");
+
+const result1 = document.getElementById("result1");
+const result1children = result1.children[0].children[1].children[2].children[0];
+console.log(result1children);
+
+const result2 = document.getElementById("result2");
+
+const result3 = document.getElementById("result3");
+
+const result4 = document.getElementById("result4");
+
+const result5 = document.getElementById("result5");
+
+const result6 = document.getElementById("result6");
+
+const result7 = document.getElementById("result7");
+
+const result8 = document.getElementById("result8");
+
+const result9 = document.getElementById("result9");
+
+const result10 = document.getElementById("result10");
+
+const results = [
+  result1,
+  result2,
+  result3,
+  result4,
+  result5,
+  result6,
+  result7,
+  result8,
+  result9,
+  result10,
+];
+
+button.addEventListener("click", func);
+addEventListener("keydown", checkKey);
+
+function checkKey(e) {
+  if (e.key == "Enter") {
+    func();
+  }
+}
+
+async function func() {
+  let title = document.getElementById("search").value;
+  let search_title = "";
+  for (let i = 0; i < title.length; i++) {
+    if (title[i] == " ") {
+      search_title += "+";
+    } else {
+      search_title += title[i];
+    }
+  }
+  try {
+    const response = await fetch(endpoint + search_title);
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    const json = await response.json();
+    console.log("original json");
+    console.log(json);
+    if (json["Error"]) {
+      alert(json["Error"]);
+    } else {
+      for (let i = 0; i < 10; i++) {
+        const response2 = await fetch(
+          endpoint2 +
+            json["Search"][i]["Title"] +
+            "&y=" +
+            json["Search"][i]["Year"] +
+            "&type=" +
+            json["Search"][i]["Type"]
+        );
+        const json2 = await response2.json();
+        json["Search"][i] = json2;
+      }
+      loadresults(json);
+      localStorage.setItem("current_poster", json["Search"][0]["Poster"]);
+    }
+  } catch (err) {
+    console.log(err);
+    alert("Failed to get movie");
+  }
+  showresults();
+}
+
+function showresults() {
+  for(let i = 0; i < 10; i++)
+  {
+    results[i].classList.add("shown");
+    results[i].classList.remove("hidden");
+  }
+}
+
+function loadresults(json) {
+  console.log(json["Search"][0]);
+  for (let i = 0; i < 10; i++) {
+    results[i].children[0].children[0].src = json["Search"][i]["Poster"];
+    results[i].children[0].children[1].children[0].textContent = json["Search"][i]["Title"];
+    results[i].children[0].children[1].children[1].children[0].textContent = json["Search"][i]["Year"];
+    results[i].children[0].children[1].children[1].children[2].textContent = json["Search"][i]["Rated"];
+    if (json["Search"][i]["Type"] == "series") {
+      console.log("this is a series");
+      results[i].children[0].children[1].children[1].children[3].textContent = "TV Series";
+      results[i].children[0].children[1].children[1].children[1].classList.add("hidden");
+      results[i].children[0].children[1].children[1].children[1].classList.remove("shown");
+    } else {
+      results[i].children[0].children[1].children[1].children[3].textContent = json["Search"][i]["Metascore"] + " Metascore";
+      results[i].children[0].children[1].children[1].children[1].classList.add("shown");
+      results[i].children[0].children[1].children[1].children[1].classList.remove("hidden");
+      results[i].children[0].children[1].children[1].children[1].textContent = json["Search"][i]["Runtime"];
+    }
+    results[i].children[0].children[1].children[2].children[0].textContent = json["Search"][i]["imdbRating"] + " Stars";
+  }
+}
